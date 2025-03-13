082674e89d52bac2df0d11183fe8a9bd148ca12c
Ahmed Mohamed Moqawi <ahmedmuqawi@gmail.com>
2025-03-13 21:23:33 +0200

    fix(model): fix the image path and is_stock_critical method

--- products/models.py
+++ products/models.py
@@ -3,12 +3,15 @@ from django.utils.text import slugify
 from django.core.validators import MinValueValidator
 from django.urls import reverse
 
+
+def product_image_path(instance, filename):
+    return f'products/images/{instance.slug}/{filename}'
 # Product Model
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(max_length=255, unique=True)
-    image = models.ImageField(upload_to='products/images/')
+    image = models.ImageField(upload_to=product_image_path, blank=True)
     current_quantity = models.PositiveIntegerField(default=0,validators=[MinValueValidator(0)])
     critical_quantity = models.PositiveIntegerField(default=0,validators=[MinValueValidator(0)])
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,7 +19,7 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            self.slug = slugify(f"{self.name}")
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -26,4 +29,4 @@ class Product(models.Model):
         return reverse('product-detail', kwargs={'slug': self.slug})
 
     def is_stock_critical(self):
-        return self.current_quantity < self.critical_quantity
+        return 0 < self.current_quantity < self.critical_quantity
