172aec07144e6f0dd7ade5ad18076a80be52661f
Mohgar <mohamedhgar007@gmail.com>
2025-03-14 17:13:03 +0200

    feat(views,urls): create product list view , implement pagination and sarching

--- products/views.py
+++ products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Product
+
+
+class ProductListView(LoginRequiredMixin, ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 9
+    context_object_name = 'products'
+    login_url = '/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['current_page'] = self.request.GET.get('page', 1)
+        return context
+
+    def get_queryset(self):
+        try:
+            query_set = super().get_queryset()
+            search_query = self.request.GET.get('search')
+            latest_only = self.request.GET.get('latest')
+            if search_query:
+                query_set = query_set.filter(Q(name__iconatin=search_query) | Q(description__icontains=search_query))
+
+            if latest_only:
+                query_set = query_set.order_by('-created_at')
+
+            return query_set
+        except Exception as e:
+            print(f"Error filtering products: {e}")
+            return Product.objects.none()
 
 # Create your views here.
