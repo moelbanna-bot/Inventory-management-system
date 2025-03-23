d671d46dc32ef8e88d5d7b6a6cb5b6b1847e7ac6
Mohamed Elbanna <moelbanna880@gmail.com>
2025-03-23 15:41:02 +0200

    added query to get count of shipments from the supplier

--- shipments/views.py
+++ shipments/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
+from django.db.models import Q, Count
 from .models import Supplier, Shipment
 
 
 class SupplierListView(LoginRequiredMixin, ListView):
     model = Supplier
-    template_name = "shipments.html"
+    template_name = "shipments/suppliers.html"
     context_object_name = "suppliers"
     ordering = ["name"]
     login_url = "login"
@@ -14,14 +14,15 @@ class SupplierListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add shipments to the context for the template
-        context["shipments"] = Shipment.objects.all().order_by("-created_at")[:10]
         context["current_page"] = self.request.GET.get("page", 1)
         return context
 
     def get_queryset(self):
         try:
             query_set = super().get_queryset().filter(is_active=True)
+            query_set = query_set.annotate(
+                total_purchase_shipments=Count("shipment", distinct=True),
+            )
             search_query = self.request.GET.get("search")
 
             if search_query:
