98f28880cbd0e0644eb856b25f917cce282ced04
KhaledGaafar <khaledgafaar211@gmail.com>
2025-03-23 15:35:23 +0200

    feat(Config Email and User password life cycle):create a configuration email for system and management user reset and confirm password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
+from .views import register,CustomPasswordResetView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
