ecd4548a1de4f5346f3833a3b99a23be2bb507e6
Mohamed Elbanna <moelbanna880@gmail.com>
2025-03-25 00:07:27 +0200

    Refactor views.py for consistent string formatting and improved readability

--- accounts/views.py
+++ accounts/views.py
@@ -12,20 +12,26 @@ from .permissions import is_manager
 
 
 def send_activate_email(request, user):
-    protocol = 'https' if request.is_secure() else 'http'
+    protocol = "https" if request.is_secure() else "http"
     domain = request.get_host()
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
     print(f"Sending email to: {user.email}")
-    subject = 'Welcome to our Inventory'
-    plain_message = 'Welcome to our Inventory! Your account has been successfully created.'
-    html_message = render_to_string('accounts/emails/welcome.html', {'user': user,
-                                                                     'protocol': protocol,
-                                                                     'domain': domain,
-                                                                     'uid': uid,
-                                                                     'token': token,
-                                                                     })
-    from_email = 'khaledgafaar211@gmail.com'
+    subject = "Welcome to our Inventory"
+    plain_message = (
+        "Welcome to our Inventory! Your account has been successfully created."
+    )
+    html_message = render_to_string(
+        "accounts/emails/welcome.html",
+        {
+            "user": user,
+            "protocol": protocol,
+            "domain": domain,
+            "uid": uid,
+            "token": token,
+        },
+    )
+    from_email = "khaledgafaar211@gmail.com"
     to_email = user.email
 
     try:
@@ -43,40 +49,39 @@ def send_activate_email(request, user):
 
 def register(request):
     if not is_manager(request.user):
-        return render(request, '403.html', status=403)
+        return render(request, "403.html", status=403)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             try:
                 user = form.save()
                 send_activate_email(request, user)
-                messages.success(request, 'Account created successfully')
-                return redirect('home')
+                messages.success(request, "Account created successfully")
+                return redirect("home")
             except Exception as e:
-                messages.error(request, 'An error occurred')
-
+                messages.error(request, "An error occurred")
 
     else:
         form = UserRegisterForm()
 
-    return render(request, 'accounts/register_user.html', {'form': form})
+    return render(request, "accounts/register_user.html", {"form": form})
 
 
 class CustomPasswordResetView(PasswordResetView):
     form_class = CustomPasswordResetForm
-    template_name = 'registration/password_reset_form.html'
-    success_url = '../password_reset/done/'
+    template_name = "registration/password_reset_form.html"
+    success_url = "../password_reset/done/"
 
 
 class CustomLoginView(LoginView):
-    template_name = 'accounts/login.html'  # Adjust to match your template path
+    template_name = "accounts/login.html"  # Adjust to match your template path
 
     def dispatch(self, request, *args, **kwargs):
         # If the user is already authenticated, redirect them
         if request.user.is_authenticated:
             # Redirect to your desired URL (home, dashboard, etc.)
-            return redirect('home')  # Change 'home' to your homepage URL name
+            return redirect("home")  # Change 'home' to your homepage URL name
 
         # Otherwise, proceed with the normal login view
         return super().dispatch(request, *args, **kwargs)
