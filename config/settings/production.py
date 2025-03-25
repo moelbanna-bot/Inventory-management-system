f3205fbc9e23ce80f97cb19a6959c7060cd35e35
Mohamed Elbanna <moelbanna880@gmail.com>
2025-03-25 15:41:51 +0200

    test media

--- config/settings/production.py
+++ config/settings/production.py
@@ -42,6 +42,8 @@ SECURE_BROWSER_XSS_FILTER = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Database configuration
 
